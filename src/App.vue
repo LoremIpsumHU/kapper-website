148da693262efa61b4e15f6ead083f539fdde734
Luuk van Dijk <luukvandijk2003@gmail.com>
2021-12-01 13:45:42 +0100

    personeel cards

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,10 @@
 
 <script>
 import Stappenplan from '../src/components/stappenplan.vue'
-import Personeel from '../src/components/Personeel.vue'
 
 export default {
   components: {
     'stappenplan': Stappenplan,
-    'personeel': Personeel,
   },
   data(){
     return{
--- src/components/Personeel.vue
+++ src/components/Personeel.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="colmn">
     <div id="col">
-      <div id="Alisha">
+      <div id="Alisha" class="card">
         <img src="src/assets/Alisha.png" alt="Alisha">
+        <div class="card-info">
+          <div class="names">Alisha</div>
+          <div>Puntjes bij knippen <br> Nutteloze stagair</div>
+        </div>
+          
       </div>
-      <div id="Kevin">
+      <div id="Kevin" class="card">
         <img src="src/assets/Kevin.png" alt="Kevin">
+        <div class="card-info">
+          <div class="names">Kevin</div>
+          <div>Haar kleuren</div>
+        </div>
       </div>
     </div>
 
     <div id="col">
-      <div id="Pieter">
+      <div id="Pieter" class="card">
       <img src="src/assets/Pieter.png" alt="Pieter">
+      <div class="card-info">  
+        <div class="names">Pieter</div>
+        <div>Dames <br> Praten</div>
+      </div>
     </div>
-    <div id="Tobias">
+    <div id="Tobias" class="card">
       <img src="src/assets/Tobias.png" alt="Tobias">
+      <div class="card-info">
+        <div class="names">Tobias</div>
+        <div>Baarden</div>
+      </div>
     </div>
   </div>
     </div>
@@ -38,7 +55,24 @@ export default {
 }
 
 img{
-  width: 100%;
+  width: 100px;
   height: 100px;
+}
+
+.card{
+  display: flex;
+  flex-direction: row;
+  padding: 5px;
+  background: #3498DB;
+  width: 33vw;
+  margin-top: 10px;
+}
+
+.names{
+  font-size: 2em;
+}
+
+.card-info{
+  padding-left: 10px;
 }
 </style>
