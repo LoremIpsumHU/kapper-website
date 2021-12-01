96e50c44f55be0a3184579c2150c2509bb6f4875
Luuk van Dijk <luukvandijk2003@gmail.com>
2021-12-01 13:20:52 +0100

    personeel foto's

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
     <stappenplan></stappenplan>
+    <personeel></personeel>
   </div>
 </template>
 
 <script>
 import Stappenplan from '../src/components/stappenplan.vue'
+import Personeel from '../src/components/Personeel.vue'
+
 export default {
   components: {
-    Stappenplan
+    'stappenplan': Stappenplan,
+    'personeel': Personeel,
+  },
+  data(){
+    return{
+
+    }
   }
 }
 </script>
--- src/components/Personeel.vue
+++ src/components/Personeel.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
-    
+  <div id="colmn">
+    <div id="col">
+      <div id="Alisha">
+        <img src="src/assets/Alisha.png" alt="Alisha">
+      </div>
+      <div id="Kevin">
+        <img src="src/assets/Kevin.png" alt="Kevin">
+      </div>
+    </div>
+
+    <div id="col">
+      <div id="Pieter">
+      <img src="src/assets/Pieter.png" alt="Pieter">
+    </div>
+    <div id="Tobias">
+      <img src="src/assets/Tobias.png" alt="Tobias">
+    </div>
   </div>
+    </div>
 </template>
 
 <script>
 export default {
-  
+  data(){
+    return{
+
+    }
+  }
 }
 </script>
 
 <style>
-  
+#colmn{
+  display: flex;
+  flex-direction: row;
+  gap: 5em;
+}
+
+img{
+  width: 100%;
+  height: 100px;
+}
 </style>
