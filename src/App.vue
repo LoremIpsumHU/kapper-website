7229b5e8bdd725e4da31491ec95d50731ad550de
Luuk van Dijk <luukvandijk2003@gmail.com>
2021-12-01 12:16:55 +0100

    stappenplan

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    
+    <stappenplan></stappenplan>
   </div>
 </template>
 
 <script>
+import Stappenplan from '../src/components/stappenplan.vue'
 export default {
-  
+  components: {
+    Stappenplan
+  }
 }
 </script>
 
 <style>
-  
+
 </style>
---
+++ src/components/stappenplan.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div id="stappen">
+      <div class="stap" id="slected">Stap 1 <br> Selecteren</div>
+      <div class="stap">Stap 2 <br> Datum</div>
+      <div class="stap">Stap 3 <br> Kapper</div>
+      <div class="stap">Stap 4 <br> Gegevens</div>
+      <div class="stap">Stap 5 <br> Bevestigen</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        stap1: 'Stap 1 Selecteren',
+        stap2: 'Stap 2 Datum',
+        stap3: 'Stap 3 Kapper',
+        stap4: 'Stap 4 Gegevens', 
+        stap5: 'Stap 5 Bevestigen',
+        uitleg1: 'Slecteren',
+        uitleg2: 'Datum',
+        uitleg3: 'Kapper',
+        uitleg4: 'Gegevens',
+        uitleg5: 'Bevestigen'
+      }
+    }
+}
+</script>
+
+<style scoped>
+div{
+  padding: 0 0 0;
+}
+
+#stappen {
+  display: flex;
+  gap: 2em;
+  }
+
+.stap {
+  padding: 1em;
+}
+
+#slected{
+  color: #CF4D36;
+}
+</style>
