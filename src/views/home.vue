e878a4d47bed8b021c591056283b5c126032e119
moos.mostert <moos.mostert@student.hu.nl>
2022-01-17 12:10:09 +0100

    foot

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,19 +3,21 @@
   <hero></hero>
   <kapsels></kapsels>
   <info></info>
+  <foot></foot>
 </div>
 </template>
-
 <script>
 import hero from '../components/homepage/hero.vue'
 import info from '../components/homepage/info.vue'
 import kapsels from '../components/homepage/kapsels.vue'
+import foot from '../components/homepage/footer.vue'
 
 export default {
   components:{
     'hero': hero,
     'info': info,
     'kapsels' : kapsels,
+    'foot' : foot,
   },
 
   data(){
